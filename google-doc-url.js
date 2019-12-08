6c746857236a38a5088338e0042986dc5c26755e
BlChrs <iu@tipmanager.com>
2019-12-08 14:17:42 +0100

    changed google sheet url

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1UWRiVZRMzNMle74DJ85qGTChR4tQxRWIVWfS8t7p5Vc/';
